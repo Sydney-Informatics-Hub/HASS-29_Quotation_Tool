1ec8275b05d2faa18598307f4733684d229fa6b5
Sony Jufri <sony.jufri@sydney.edu.au>
2022-07-22 11:02:36 +1000

    Update extract_display_quotes.py

--- extract_display_quotes.py
+++ extract_display_quotes.py
@@ -58,6 +58,30 @@ from quote_extractor import extract_quotes
 import utils
 
 
+class DownloadFileLink(FileLink):
+    html_link_str = "<a href='{link}' download={file_name}>{link_text}</a>"
+
+    def __init__(self, path, file_name=None, link_text=None, *args, **kwargs):
+        super(DownloadFileLink, self).__init__(path, *args, **kwargs)
+
+        self.file_name = file_name or os.path.split(path)[1]
+        self.link_text = link_text or self.file_name
+
+    def _format_path(self):
+        from html import escape
+
+        fp = "".join([self.url_prefix, escape(self.path)])
+        return "".join(
+            [
+                self.result_html_prefix,
+                self.html_link_str.format(
+                    link=fp, file_name=self.file_name, link_text=self.link_text
+                ),
+                self.result_html_suffix,
+            ]
+        )
+
+
 class QuotationTool():
     '''
     Interactive tool for extracting and displaying quotes in a text
@@ -504,9 +528,11 @@ class QuotationTool():
                     file.close()
                     clear_output()
                     print('Preview saved!')
-                    from IPython.display import FileLink
-                    local_file = FileLink(out_dir+str(text_name)+'.html', result_html_prefix='Click here to download: ')
-                    display(local_file)
+                    import DownloadFileLink
+                    DownloadFileLink(out_dir+str(text_name)+'.html', "Download")
+                    #from IPython.display import FileLink
+                    #local_file = FileLink(out_dir+str(text_name)+'.html', result_html_prefix='Click here to download: ')
+                    #display(local_file)
                 except:
                     print('You need to generate a preview before you can save it!')
         
